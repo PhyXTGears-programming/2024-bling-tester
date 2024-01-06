cae4e6a6c9925a5f691e2b1d6c8e38fa4259b7ed
Ian Blair <private>
2024-01-06 07:51:36 -0500

    1/6/24 7:51 am

--- oop_display/oop_display.ino
+++ oop_display/oop_display.ino
@@ -11,8 +11,10 @@
 #define TFT_SCLK      18  // Clock out
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-float p = 3.1415926;
+// Display has x0, y0 top left; x is increasing right; y is increasing down
+
 int short_delay = 200;
+int long_delay = 4000;
 
 class Rectangle
 {
@@ -25,11 +27,16 @@ class Rectangle
   int height;           // Height of rectangle (y-direction)
   uint16_t borderColor; // Border color
   uint16_t fillColor;   // Fill color
+  int16_t cursorx;      // Cursor x
+  int16_t cursory;      // Cursor y
+  int8_t textSize;      // Text Size
+  uint16_t textColor;   // Text color
+  String screenText;    // Screen Text
 
-  // Constructor - creates a Rectangle
-  // and initializes the member variables and state
+  // Constructor - creates a Rectangle with text
+  // Initializes the member variables
   public:
-  Rectangle(int x, int y,int b,int h,int w,uint16_t bc,uint16_t fc)
+  Rectangle(int x,int y,int b,int h,int w,uint16_t bc,uint16_t fc,String txt,uint16_t tc, int ts)
   {
   x0 = x;
   y0 = y;
@@ -38,17 +45,26 @@ class Rectangle
   width = w;
   borderColor = bc;
   fillColor = fc;
+  screenText = String(txt);
+  textSize = ts;
+  textColor = tc;
   tft.fillRect(x0,y0,width,height,borderColor);
   int innerX = x0 + border;
   int innerY = y0 + border;
   int innerW = width - 2 * border;
   int innerH = height - 2 * border;
   tft.fillRect(innerX,innerY,innerW,innerH,fillColor);
+  cursorx = x0 + 20;
+  cursorx = y0 + 20;
+  tft.setCursor(cursorx, cursory);
+  tft.setTextSize(textSize);
+  tft.setTextColor(textColor);
+  tft.println(screenText);
   }
 
-  void Delete()
+  void Delete(uint16_t screenColor)
   {
-    tft.fillRect(x0,y0,width,height,ST77XX_BLACK);
+    tft.fillRect(x0,y0,width,height,screenColor);
   }
 };
 
@@ -79,9 +95,9 @@ class Circle
   tft.fillCircle(x0,y0,innerR,fillColor);
   }
   
-  void Delete()
+  void Delete(uint16_t screenColor)
   {
-    tft.fillCircle(x0,y0,radius,ST77XX_BLACK);
+    tft.fillCircle(x0,y0,radius,screenColor);
   }
 };
 
@@ -143,26 +159,26 @@ void setup(void) {
   tft.setSPISpeed(40000000); // Max try 62,500,000
   
   tft.fillScreen(ST77XX_BLACK);
-  Text txt1(10,10,3,ST77XX_RED);
+  //Text txt1(10,10,3,ST77XX_RED);
 
-  Rectangle rect1(0,0,10,75,75,ST77XX_RED,ST77XX_GREEN);
+  Rectangle rect1(0,0,10,75,75,ST77XX_RED,ST77XX_GREEN,"abc",ST77XX_BLUE,2);
   delay(short_delay);
-  Rectangle rect2(75,75,10,75,75,ST77XX_BLUE,ST77XX_YELLOW);
+  Rectangle rect2(75,75,10,75,75,ST77XX_BLUE,ST77XX_YELLOW,"xyz",ST77XX_RED,2);
   delay(short_delay);
   Circle circ1(200,40,5,38,ST77XX_RED,ST77XX_GREEN);
   delay(short_delay);
   Circle circ2(280,115,5,38,ST77XX_BLUE,ST77XX_YELLOW);
-  txt1.Update(10,10,3,ST77XX_RED);
+  //txt1.Update(10,10,3,ST77XX_RED);
+  //delay(4000);
+  //txt1.Delete();
   delay(4000);
-  txt1.Delete();
-  delay(4000);
-  rect1.Delete();
+  rect1.Delete(ST77XX_BLACK);
   delay(short_delay);
-  rect2.Delete();
+  rect2.Delete(ST77XX_BLACK);
   delay(short_delay);
-  circ1.Delete();
+  circ1.Delete(ST77XX_BLACK);
   delay(short_delay);
-  circ2.Delete();
+  circ2.Delete(ST77XX_BLACK);
   delay(5000);
   
   Serial.println("done");
