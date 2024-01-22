3664091e75ce722307faa2694d84780f7d286257
Ian Blair <private>
2024-01-22 15:54:59 -0500

    1/22/24 3:54

--- examples/pico_button/pico_button.ino
+++ examples/pico_button/pico_button.ino
@@ -1,33 +1,8 @@
-/*
-  Button
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground through 220 ohm resistor
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
-*/
-
-// constants won't change. They're used here to set pin numbers:
 const int buttonPin = 14;     // the number of the pushbutton pin
 const int ledPin =  13;      // the number of the LED pin
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+bool ledBlink = false;
 
 void setup() {
   // initialize the LED pin as an output:
@@ -38,14 +13,10 @@ void setup() {
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+  if(digitalRead(buttonPin)==true){
+    ledBlink = !ledBlink;
+  } while(digitalRead(buttonPin)==true);
+    digitalWrite(ledPin, ledBlink);
+    delay(50);
   }
-}
+ 
